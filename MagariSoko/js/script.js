f99e8ccf6a6703d5d00974bce3e9a7fe58f7fabf
Basil <57216667+basil809@users.noreply.github.com>
2024-07-16 14:48:11 +0300

    Add files via upload

---
+++ MagariSoko/js/script.js
@@ -0,0 +1,9 @@
+function toggleSidebar() {
+    const sidebar = document.getElementById('sidebar');
+    sidebar.classList.toggle('open');
+}
+
+function toggleCarMakes() {
+    const carMakes = document.getElementById('car-makes');
+    carMakes.classList.toggle('hidden');
+}
